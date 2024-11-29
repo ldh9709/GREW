f55bfb554eaa85c22ae028e14dda464dbcb9ccea
ldh9709 <zszz5434@gmail.com>
2024-11-29 16:55:21 +0900

    안쓰는 어노테이션 삭제

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/entity/Member.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/entity/Member.java
@@ -2,21 +2,17 @@ package com.itwill.jpa.entity;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
-import org.hibernate.annotations.ColumnDefault;
 
 import com.itwill.jpa.dto.MemberDto;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.SequenceGenerator;
