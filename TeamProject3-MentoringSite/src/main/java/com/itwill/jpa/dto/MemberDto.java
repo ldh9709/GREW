2c1cba14c9c00784bf5f3accaa39d0a0ba28e0c1
ldh9709 <zszz5434@gmail.com>
2024-11-29 15:32:35 +0900

    MEMBER, MEMBERDTO 커밋

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/dto/MemberDto.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/dto/MemberDto.java
@@ -18,31 +18,19 @@ import net.bytebuddy.asm.Advice.Local;
 
 public class MemberDto {
 	
-	private Long memberNo;
 	private String memberId;
 	private String memberPassword;
 	private String memberEmail;
 	private String memberName;
-	private String memberRole;
-	private String memberPoints;
-	private String memberStatus;
-	private LocalDate memberJoinDate;
-	private String memberReportCount;
 	
 	
 	 /* Entitiy -> DTO*/
 	public static MemberDto toDto(Member memberEntity) {
 		return MemberDto.builder()
-	            .memberNo(memberEntity.getMemberNo())
 	            .memberId(memberEntity.getMemberId())
 	            .memberPassword(memberEntity.getMemberPassword())
 	            .memberEmail(memberEntity.getMemberEmail())
 	            .memberName(memberEntity.getMemberName())
-	            .memberRole(memberEntity.getMemberRole())
-	            .memberPoints(memberEntity.getMemberPoints())
-	            .memberStatus(memberEntity.getMemberStatus())
-	            .memberJoinDate(memberEntity.getMemberJoinDate())
-	            .memberReportCount(memberEntity.getMemberReportCount())
 	            .build();
 	}
 	
