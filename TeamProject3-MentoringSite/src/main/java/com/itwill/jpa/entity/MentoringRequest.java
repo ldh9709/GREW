39188d6a3e6d45a19221fad7596d3c976697eeea
ldh9709 <zszz5434@gmail.com>
2024-11-29 16:57:52 +0900

    ㅇㅇ

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/entity/MentoringRequest.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/entity/MentoringRequest.java
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 package com.itwill.jpa.entity;
 
 public class MentoringRequest {
 
 }
-=======
-package com.itwill.jpa.entity;
-
-public class MentoringRequest {
-
-}
->>>>>>> refs/heads/master
