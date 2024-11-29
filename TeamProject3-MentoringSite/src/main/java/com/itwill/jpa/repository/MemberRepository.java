d7da17b1e2fabc949416cda455ad89c8925700b9
ldh9709 <zszz5434@gmail.com>
2024-11-29 18:49:58 +0900

    Repository 작업

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/MemberRepository.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/MemberRepository.java
@@ -1,5 +1,21 @@
 package com.itwill.jpa.repository;
 
-public class MemberRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.itwill.jpa.entity.Member;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+	
+	//로그인 시 아이디와 비밀번호로 객체 찾기
+	public Member findMemberByMemberIdAndMemberPassword(String memberId, String memberPassword);
+	
+	//아이디로 멤버 삭제
+	public Member deleteMemberByMemberId(String memberId);
+	
+	//아이디로 멤버 조회
+	public Member findMemberByMemberId(String memberId);
+	
+	
 }
---
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/MemberServiceImpl.java
@@ -0,0 +1,80 @@
+package com.itwill.jpa.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itwill.jpa.dto.MemberDto;
+import com.itwill.jpa.entity.Member;
+import com.itwill.jpa.repository.MemberRepository;
+
+@Service
+public class MemberServiceImpl implements MemberService {
+	/***** 24.11.29 유효성 체크 및 Exception 사용 전 *****/
+	
+	
+	//메소드 사용을 위한 리포지토리 의존성 주입
+	@Autowired
+	MemberRepository memberRepository;
+	
+	/***** 회원 가입 *****/
+	@Override
+	public Member saveMember(MemberDto memberDto) {
+		
+		//MemberDto Entity로 변경
+		Member saveMember = Member.toEntity(memberDto);
+		
+		//객체 저장
+		return memberRepository.save(saveMember);
+	}
+	
+	/***** 회원 로그인 *****/
+	@Override
+	public Member loginMember(String memberId, String memberPassword) {
+		
+		//아이디와 비밀번호로 멤버 객체 찾기
+		Member member = memberRepository.findMemberByMemberIdAndMemberPassword(memberId, memberPassword);
+		
+		//member가 존재하지 않을 시
+		if(member == null) {
+			String msg = "존재하지 않는 아이디입니다.";
+		}
+		
+		//member의 비밀번호가 일치하지 않을 시
+		if(!member.getMemberPassword().equals(memberPassword)) {
+			String msg = "비밀번호가 일치하지 않습니다.";
+		}
+		
+		//다 통과하면 member반환
+		return member;
+	}
+	
+	/***** 회원 수정 ****/
+	@Override
+	public Member updateMember(Member member) {
+		
+		//DB에 객체 업데이트(기존 객체 존재 시 업데이트됨)
+		return memberRepository.save(member);
+	}
+	
+	/***** 회원 삭제 *****/
+	@Override
+	public Member deleteMember(String memberId) {
+		//ID로 멤버 찾기
+		Member findMember = memberRepository.findMemberByMemberId(memberId);
+		
+		//DB에서 멤버 객체 삭제
+		memberRepository.deleteMemberByMemberId(memberId);
+		
+		//삭제한 멤버 객체 반환
+		return findMember;
+	}
+	
+	/***** 회원 상세 *****/
+	@Override
+	public Member getMember(String memberId) {
+		
+		//ID로 멤버 탐색
+		return memberRepository.findMemberByMemberId(memberId);
+	}
+
+}
