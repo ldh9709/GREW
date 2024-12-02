85bd7ca7c6616ebdc237c21ca08e23fc9c6bf779
ldh9709 <zszz5434@gmail.com>
2024-12-02 15:58:03 +0900

    ..

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/MemberRestController.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/MemberRestController.java
@@ -1,5 +1,25 @@
 package com.itwill.jpa.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itwill.jpa.dto.user_information.MemberDto;
+import com.itwill.jpa.response.Response;
+import com.itwill.jpa.service.MemberService;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/member")
 public class MemberRestController {
+	@Autowired
+	private MemberService memberService;
+	
+	public ResponseEntity<Response> saveMember(@RequestBody MemberDto memberDto) {
+		
+	}
 
 }
