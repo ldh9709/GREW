1b7a7da15268e0d5be2fe89a74965387809a2e79
ldh9709 <zszz5434@gmail.com>
2024-12-02 09:42:32 +0900

    Member Repository, service, impl 기초

---
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/MemberService.java
@@ -0,0 +1,23 @@
+package com.itwill.jpa.service;
+
+import com.itwill.jpa.dto.user_information.MemberDto;
+import com.itwill.jpa.entity.user_information.Member;
+
+public interface MemberService {
+	
+	//회원가입
+	Member saveMember(MemberDto memberDto);
+	
+	//로그인
+	Member loginMember(String memberId, String memberPassword);
+	
+	//회원수정
+	Member updateMember(Member member);
+	
+	//회원삭제
+	Member deleteMember(String memberId);
+	
+	//회원상세
+	Member getMember(String memberId);
+	
+}
