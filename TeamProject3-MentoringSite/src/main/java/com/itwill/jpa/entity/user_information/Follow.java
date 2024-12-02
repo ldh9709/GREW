4f119975afd6ac1360ef0d57760fe663bf848157
imun46 <do2816@gmail.com>
2024-12-02 15:50:13 +0900

    Follow entity 컬럼, 테이블 명 수정

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/entity/user_information/Follow.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/entity/user_information/Follow.java
@@ -14,20 +14,23 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "Follow")
+@Table(name = "follow")
 public class Follow {
 
     @Id
     @SequenceGenerator(name = "follow_seq", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "follow_seq")
+    @Column(name = "follow_no")
     private Long followNo;
 
+    // 팔로우를 한 사용자(멘티)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "follower_member") // 팔로우를 한 사용자
+    @JoinColumn(name = "follower_member")
     private Member followerMember;
 
+ // 팔로우 대상 사용자(멘토)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "followed_member") // 팔로우 대상 사용자
+    @JoinColumn(name = "followed_member") 
     private Member followedMember;
  
     /*
