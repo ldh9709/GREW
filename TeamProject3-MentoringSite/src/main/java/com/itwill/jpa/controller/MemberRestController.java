0377cb6cd8f4607f4ead7ed5db061321f0167a84
ldh9709 <zszz5434@gmail.com>
2024-12-02 17:54:33 +0900

    ..

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/MemberRestController.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/MemberRestController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,6 +23,7 @@ import com.itwill.jpa.response.ResponseStatusCode;
 import com.itwill.jpa.service.MemberService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpSession;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -60,6 +63,65 @@ public class MemberRestController {
 		return responseEntity;
 	}
 	
+	/* 회원 로그인 */
+	@Operation(summary = "회원 로그인")
+	@PostMapping("/login")
+	public ResponseEntity<Response> loginMember(@RequestBody MemberDto memberDto, HttpSession session) {
+		
+		Member loginMmember = memberService.loginMember(memberDto.getMemberId(), memberDto.getMemberPassword());
+		
+		session.setAttribute("loginMember", loginMmember);
+		
+		//응답 객체 생성
+		Response response = new Response();
+				
+		if(loginMmember != null) {
+			//응답객체에 코드, 메시지, 객체 설정
+			response.setStatus(ResponseStatusCode.LOGIN_MEMBER_SUCCESS);
+			response.setMessage(ResponseMessage.LOGIN_MEMBER_SUCCESS);
+			response.setData(loginMmember);
+		}
+		
+		//인코딩 타입 설정
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8")));
+		
+		//반환할 응답Entity 생성
+		ResponseEntity<Response> responseEntity =
+				 new ResponseEntity<Response>(response, httpHeaders, HttpStatus.OK);
+		
+		//반환
+		return responseEntity;
+	}
 	
-
+	/* 회원 로그아웃 */
+	@Operation(summary = "회원 로그아웃")
+	@GetMapping("/logout")
+	public ResponseEntity<Response> logoutMember(HttpSession session) {
+		
+		//세션 제거
+		session.invalidate();
+		
+		Response response = new Response();
+		response.setStatus(ResponseStatusCode.LOGOUT_MEMBER_SUCCESS);
+		response.setMessage(ResponseMessage.LOGOUT_MEMBER_SUCCESS);
+		
+		HttpHeaders httpHeaders=new HttpHeaders();
+		httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON,Charset.forName("UTF-8")));
+		
+		ResponseEntity<Response> responseEntity = 
+				new ResponseEntity<Response>(response, httpHeaders, HttpStatus.OK);
+		
+		return responseEntity;
+	}
+	
+	/* 회원 정보 보기 */
+	@Operation(summary = "회원 정보 보기")
+	@GetMapping("/{memberNo}")
+	public ResponseEntity<Response> getMember(@PathVariable(name = "memberNo") Long memberNo, HttpSession session) {
+		return null;
+	}
+	
+	
+	
 }
