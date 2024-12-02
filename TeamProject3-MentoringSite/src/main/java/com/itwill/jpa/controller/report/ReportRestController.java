5faff28459ed5d0bfe25828d3660788c0418a241
imun46 <do2816@gmail.com>
2024-12-02 16:00:49 +0900

    reportController 생성

---
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/report/ReportRestController.java
@@ -0,0 +1,16 @@
+package com.itwill.jpa.controller.report;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReportRestController {
+	
+	/* 신고등록 */
+	
+	/* [어드민] 신고 상태변경(검토중) */
+	/* [어드민] 신고 상태변경(처리완료) */
+	/* 신고 출력(특정회원) */
+	
+	/* [어드민] 신고 출력(전체회원) */
+
+}
--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/ReportServiceImpl.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/ReportServiceImpl.java
@@ -20,33 +20,19 @@ public class ReportServiceImpl implements ReportService {
 	/*신고등록*/
 	@Override
 	public void saveReport(ReportDto reportDto){
-		try {
-			Report report = Report.toEntity(reportDto);
-			reportRepository.save(report);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		Report report = Report.toEntity(reportDto);
+		reportRepository.save(report);
 	}
 
-	/*신고 취소*/
-	@Override
-	public void updateReportStatusToCancel(Long reportNo) {
-		try {
-			Report report = reportRepository.findById(reportNo).get();
-			report.setReportStatus(4);
-			reportRepository.save(report);
-		}catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-	/* (어드민) 신고 상태 변경 : 검토중 */
+	/* [어드민] 신고 상태 변경 : 검토중 */
 	@Override
 	public void updateReportStatusToInProgress(Long reportNo) {
 		Report report = reportRepository.findById(reportNo).get();
 		report.setReportStatus(2);
 		reportRepository.save(report);
 	}
-	/* (어드민) 신고 상태 변경 : 처리완료 */
+	
+	/* [어드민] 신고 상태 변경 : 처리완료 */
 	@Override
 	public void updateReportStatusToResolved(Long reportNo) {
 		Report report = reportRepository.findById(reportNo).get();
@@ -65,8 +51,16 @@ public class ReportServiceImpl implements ReportService {
 		}
 		return reportDtos;
 	}
+	
+	/*신고 취소*/
+	@Override
+	public void updateReportStatusToCancel(Long reportNo) {
+			Report report = reportRepository.findById(reportNo).get();
+			report.setReportStatus(4);
+			reportRepository.save(report);
+	}
 
-	/*신고 전체 출력(어드민)*/
+	/* [어드민] 신고 전체 출력 */
 	@Override
 	public List<ReportDto> selectReportAll() {
 		List<Report> reports = reportRepository.findAll();
