24f9f210361a0e585db8226c77cb6f27e5422c5b
ldh9709 <zszz5434@gmail.com>
2024-12-02 16:52:43 +0900

    컨트롤러 진행 중

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/MemberRestController.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/MemberRestController.java
@@ -1,15 +1,26 @@
 package com.itwill.jpa.controller;
 
+import java.nio.charset.Charset;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.jpa.dto.user_information.MemberDto;
+import com.itwill.jpa.entity.user_information.Member;
 import com.itwill.jpa.response.Response;
+import com.itwill.jpa.response.ResponseMessage;
+import com.itwill.jpa.response.ResponseStatusCode;
 import com.itwill.jpa.service.MemberService;
+
+import io.swagger.v3.oas.annotations.Operation;
 
 @CrossOrigin(origins = "*")
 @RestController
@@ -18,8 +29,35 @@ public class MemberRestController {
 	@Autowired
 	private MemberService memberService;
 	
+	
+	/* 회원 저장 */
+	@Operation(summary = "회원가입")
+	@PostMapping
 	public ResponseEntity<Response> saveMember(@RequestBody MemberDto memberDto) {
 		
+		//저장메소드 실행
+		Member saveMember = memberService.saveMember(memberDto);
+		
+		//응답 객체 생성
+		Response response = new Response();
+		
+		if(saveMember != null) {
+			//응답객체에 코드, 메시지, 객체 설정
+			response.setStatus(ResponseStatusCode.CREATED_MEMBER_SUCCESS);
+			response.setMessage(ResponseMessage.CREATED_MEMBER_SUCCESS);
+			response.setData(saveMember);
+		}
+		
+		//인코딩 타입 설정
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8")));
+		
+		//반환할 응답Entity 생성
+		ResponseEntity<Response> responseEntity =
+				 new ResponseEntity<Response>(response, httpHeaders, HttpStatus.OK);
+		
+		//반환
+		return responseEntity;
 	}
 
 }
