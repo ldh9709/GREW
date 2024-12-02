210e26abd5d84222f571fba0b9034c1dae86c51b
jonadan0322 <jonadan0322>
2024-12-02 16:22:18 +0900

    Merge branch 'sdh'

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/AnswerRepository.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/AnswerRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.itwill.jpa.entity.bullentin_board.Answer;
 
 public interface AnswerRepository extends JpaRepository<Answer, Long>{
+	/*질문 하나에 달린 답변 리스트*/
 	List<Answer> findByInquiryNo(Long inquiryNo);
+	
+	/*카테고리별 답변 리스트(검색)*/
 }
--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/AnswerService.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/AnswerService.java
@@ -12,8 +12,8 @@ public interface AnswerService {
 	AnswerDto updateAnswer(AnswerDto answerDto) throws Exception;
 	//답변삭제
 	void deleteAnswer(Long answerNo) throws Exception;
-	//질문에 대한 답변 목록
+	//질문에 대한 답변 목록 inactive 조건 추가해야함.
 	List<AnswerDto> selectAnswerByInquiryNo(Long inquiryNo);
-	//전체 답변 목록
+	//전체 답변 목록 inactive 조건 추가해야함.
 	List<AnswerDto> selectAnswerAll();
 }
