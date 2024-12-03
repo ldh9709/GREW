cd8181fb95ab1cfc3984d57a9053d64e31907cd6
jonadan0322 <jonadan0322>
2024-12-03 09:44:08 +0900

    ResponseStatusCode,Message Inquiry,Answer 추가

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/ResponseStatusCode.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/ResponseStatusCode.java
@@ -19,6 +19,28 @@ public class ResponseStatusCode {
 	public static final int UPDATE_REPORT_SUCCESS = 4300;
 	public static final int REPORT_FAIL = 4400;
 	
+	/**********************INQUIRY 메시지**************************/
+	public static final int CREATED_INQUIRY_SUCCESS = 5000;
+	public static final int CREATED_INQUIRY_FAIL = 5100;
+	public static final int UPDATE_INQUIRY_SUCCESS = 5200;
+	public static final int UPDATE_INQUIRY_FAIL = 5300;
+	public static final int DELETE_INQUIRY_SUCCESS = 5400;
+	public static final int DELETE_INQUIRY_FAIL = 5500;
+	public static final int READ_INQUIRY_LIST_SUCCESS = 5600;
+	public static final int READ_INQUIRY_LIST_FAIL = 5700;
+	
+	/**********************ANSWER 메시지**************************/
+	public static final int CREATED_ANSWER_SUCCESS = 6000;
+	public static final int CREATED_ANSWER_FAIL = 6100;
+	public static final int UPDATE_ANSWER_SUCCESS = 6200;
+	public static final int UPDATE_ANSWER_FAIL = 6300;
+	public static final int DELETE_ANSWER_SUCCESS = 6400;
+	public static final int DELETE_ANSWER_FAIL = 6500;
+	public static final int READ_ANSWER_LIST_SUCCESS = 6600;
+	public static final int READ_ANSWER_LIST_FAIL = 6700;
+	
+	
+	
 	public static final int LOGIN_FAIL_NOT_FOUND_MEMBER = 8000;
 	public static final int LOGIN_FAIL_PASSWORD_MISMATCH_MEMBER = 8100;
 	public static final int CREATE_FAIL_EXISTED_MEMBER = 8200;
