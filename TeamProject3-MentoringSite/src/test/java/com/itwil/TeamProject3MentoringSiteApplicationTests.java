f959e7b6b32ffb859c44e1e1fe927e55998349cc
ldh9709 <zszz5434@gmail.com>
2024-12-03 13:58:58 +0900

    mmm

--- TeamProject3-MentoringSite/src/test/java/com/itwil/TeamProject3MentoringSiteApplicationTests.java
+++ TeamProject3-MentoringSite/src/test/java/com/itwil/TeamProject3MentoringSiteApplicationTests.java
@@ -5,29 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.itwill.TeamProject3MentoringSiteApplication;
-import com.itwill.jpa.dto.user_information.MemberDto;
-import com.itwill.jpa.service.MemberServiceImpl;
 
 @SpringBootTest(classes = TeamProject3MentoringSiteApplication.class)
 class TeamProject3MentoringSiteApplicationTests {
-	@Autowired
-	MemberServiceImpl memberServiceImpl;
 	
 	@Test
 	void contextLoads() {
-	}
-	
-	@Test
-	void saveMember() {
-		MemberDto memberDto = MemberDto.builder()
-				
-				.memberId("mem1")
-				.memberPassword("111")
-				.memberName("mem")
-				.memberEmail("m@m")
-				.build();
-		
-		memberServiceImpl.saveMember(memberDto);
 	}
 	
 }
