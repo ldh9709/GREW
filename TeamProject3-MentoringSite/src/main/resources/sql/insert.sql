b0e17d1a888ad96bcac8befd1ebd291a1c9671e5
apk1207 <sjaqjaos1@gmail.com>
2024-12-03 16:36:36 +0900

    insert.sql 업데이트

--- TeamProject3-MentoringSite/src/main/resources/sql/insert.sql
+++ TeamProject3-MentoringSite/src/main/resources/sql/insert.sql
@@ -35,4 +35,31 @@ VALUES (report_no_SEQ.nextval, 'USER' , 3, 3,'광고쟁이', sysdate, 1, sysdate
 INSERT INTO report(report_no,  report_type, report_target,report_reason, report_content, report_date, report_status, resolved_date, member_no) 
 VALUES (report_no_SEQ.nextval, 'INQUIRY', 1, 4,'욕쟁이', sysdate, 1, sysdate , 3);
 
+/* 카테고리 등록 */
+insert into category(category_no,category_name,category_level)
+values(category_no_SEQ.nextval, '경제', '1');
+insert into category(category_no,category_name,category_level)
+values(category_no_SEQ.nextval, '자기개발', '2');
+insert into category(category_no,category_name,category_level)
+values(category_no_SEQ.nextval, '프로그래밍', '3');
+
+/* 질문 등록 */
+insert into inquiry(inquiry_no,inquiry_title, inquiry_content, inquiry_date, inquiry_status, inquiry_views, category_no, member_no)
+values(inquiry_no_SEQ.nextval, '부자가 되고 싶어요!', '부자가될려면 어떻게 해야하나요?',sysdate,1,0,1,1);
+insert into inquiry(inquiry_no,inquiry_title, inquiry_content, inquiry_date, inquiry_status, inquiry_views, category_no, member_no)
+values(inquiry_no_SEQ.nextval, '지식을 늘리고 싶어요', '지식을 늘리는 방법에는 무엇이 있나요?',sysdate,1,0,2,2);
+insert into inquiry(inquiry_no,inquiry_title, inquiry_content, inquiry_date, inquiry_status, inquiry_views, category_no, member_no)
+values(inquiry_no_SEQ.nextval, '웹개발자 관련 질물', '웹개발자가 되려면 어떤언어를 공부하면 될까요?',sysdate,1,0,1,1);
+
+/* 답변 등록 */
+insert into answer(answer_no, answer_content, answer_date, answer_accept, answer_status, member_no, inquiry_no)
+values(answer_no_SEQ.nextval, '비트코인투자를 시작하세요!!',sysdate, 1, 1, 7, 1);
+insert into answer(answer_no, answer_content, answer_date, answer_accept, answer_status, member_no, inquiry_no)
+values(answer_no_SEQ.nextval, '은행적금을 활용해보세요',sysdate, 2, 1, 8, 1);
+insert into answer(answer_no, answer_content, answer_date, answer_accept, answer_status, member_no, inquiry_no)
+values(answer_no_SEQ.nextval, '독서와, 신문, 뉴스를 자주보세요',sysdate, 1, 1, 8, 2);
+insert into answer(answer_no, answer_content, answer_date, answer_accept, answer_status, member_no, inquiry_no)
+values(answer_no_SEQ.nextval, 'JAVA를 배우세요 추천합니다!',sysdate, 1, 1, 3, 3);
+
+
 commit;
