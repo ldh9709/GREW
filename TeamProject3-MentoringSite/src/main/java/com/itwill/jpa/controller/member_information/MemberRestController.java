476fc51086849602105c7f38cfc52aaa2eff1c20
ldh9709 <zszz5434@gmail.com>
2024-12-05 19:42:54 +0900

    ,,

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/member_information/MemberRestController.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/member_information/MemberRestController.java
@@ -36,7 +36,7 @@ public class MemberRestController {
 	
 	
 	/* 회원 저장 */
-	@Operation(summary = "회원가입")
+	@Operation(summary = "회원가입/관심사 입력")
 	@PostMapping
 	public ResponseEntity<Response> saveMember(@RequestBody MemberDto memberDto) {
 		
