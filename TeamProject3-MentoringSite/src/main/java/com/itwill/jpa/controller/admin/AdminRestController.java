d2793d900e4829c466bd4d9c2279ec539277452f
hornheart <hornheart@naver.com>
2024-12-12 17:23:11 +0900

    chore : (gitignore)merge

---
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/admin/AdminRestController.java
@@ -0,0 +1,101 @@
+package com.itwill.jpa.controller.admin;
+
+import java.nio.charset.Charset;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.itwill.jpa.dto.bulletin_board.InquiryDto;
+import com.itwill.jpa.response.Response;
+import com.itwill.jpa.response.ResponseMessage;
+import com.itwill.jpa.response.ResponseStatusCode;
+import com.itwill.jpa.service.bullentin_board.AnswerService;
+import com.itwill.jpa.service.bullentin_board.AnswerServiceImpl;
+import com.itwill.jpa.service.bullentin_board.InquiryService;
+
+import io.swagger.v3.oas.annotations.Operation;
+
+@RestController
+@RequestMapping("/admin")
+public class AdminRestController {
+	
+	@Autowired
+	private InquiryService inquiryService;
+	
+	@Autowired
+	private  AnswerService answerService;
+
+	/************* 질문 ************/
+	/* (질문)게시글 전체출력 
+	 *  INQURIRY :getByAllInquiryOrderByDate
+	 *
+	 * */
+	@Operation(summary = "질문 게시글 전체 출력(최신순)")
+	@GetMapping("/Inquiry")
+	public ResponseEntity<Response> getAdminInquiriesOrderByDate(
+			@RequestParam(name = "page",defaultValue = "0") int page,  // 기본값은 0 페이지
+			@RequestParam(name = "size",defaultValue = "10") int size) {
+		Page<InquiryDto> inquiryDtos = inquiryService.getByAllInquiryOrderByDate(page,size);
+		
+		Response response = new Response();
+		response.setStatus(ResponseStatusCode.READ_INQUIRY_LIST_SUCCESS);
+		response.setMessage(ResponseMessage.READ_INQUIRY_LIST_SUCCESS);
+		response.setData(inquiryDtos);
+		
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON,Charset.forName("UTF-8")));
+		
+		ResponseEntity<Response> responseEntity = 
+				new ResponseEntity<Response>(response, httpHeaders, HttpStatus.OK);
+		
+		return responseEntity;
+	}
+	
+	/* 카테고리별 출력 
+	 * getByCategoryInquiryOrderByDate*/
+	@Operation(summary = "관리자가 카테고리별 최신 순으로 질문을 출력")
+	@GetMapping("/Date/{categoryNo}")
+	public ResponseEntity<Response> getAdminCategoryInquiriesSortedByDate(@PathVariable(name = "categoryNo") Long categoryNo,
+			@RequestParam(name = "page",defaultValue = "0") int page,  // 기본값은 0 페이지
+			@RequestParam(name = "size",defaultValue = "10") int size) {
+			// 카테고리별 최신 순으로 질문 목록을 가져옴
+		Page<InquiryDto> inquiryDtos = inquiryService.getByCategoryInquiryOrderByDate(categoryNo,page,size);
+		 // 응답 객체 생성 및 상태 코드 설정
+		Response response = new Response();
+		response.setStatus(ResponseStatusCode.READ_INQUIRY_LIST_SUCCESS);// 상태 코드: 성공
+		response.setMessage(ResponseMessage.READ_INQUIRY_LIST_SUCCESS);// 메시지: 성공
+		response.setData(inquiryDtos);// 데이터 설정
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON,Charset.forName("UTF-8")));
+		// 응답 반환
+		ResponseEntity<Response> responseEntity = 
+				new ResponseEntity<Response>(response, httpHeaders, HttpStatus.OK);
+		
+		return responseEntity;
+	}
+	
+	/* 검색 내용 별 출력 (inquiry)*/
+	
+	
+	/* 검색 내용 별 출력 (answer)*/
+	
+	
+	
+	/* 삭제(상태변경) */
+
+
+	/* (멘토컨텐츠)게시글 전체출력 */
+	
+	/* 검색내용 별 출력 */
+	
+	
+}
