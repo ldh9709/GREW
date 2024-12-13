c2b3813d74455e7200f74ada37bb50d648a5adff
hornheart <hornheart@naver.com>
2024-12-13 15:05:48 +0900

    docs:(관리자) Add package

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/admin/AdminSelectReportController.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/admin/AdminSelectReportController.java
@@ -1,12 +1,14 @@
 package com.itwill.jpa.controller.admin;
 
 import java.nio.charset.Charset;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.itwill.jpa.dto.report.ReportDto;
 import com.itwill.jpa.response.Response;
+import com.itwill.jpa.response.ResponseMessage;
 import com.itwill.jpa.response.ResponseStatusCode;
 import com.itwill.jpa.service.report.ReportService;
 
@@ -26,6 +29,34 @@ public class AdminSelectReportController {
 	
 	@Autowired
 	private ReportService reportService;
+	
+	/* [어드민] 신고 출력(전체회원) */
+	@Operation(summary = "[어드민] 전체 신고 목록 조회")
+	@GetMapping("/Reports")
+	public ResponseEntity<Response> getAdminReportList(
+	        @RequestParam(name = "filter") Integer filter,
+	        @RequestParam(name = "page", defaultValue = "0") int page,
+	        @RequestParam(name = "size", defaultValue = "10") int size) {
+	    
+	    // 신고 목록을 가져오는 서비스 메서드 호출
+	    List<ReportDto> reports = reportService.getReportAll(filter, page, size); 
+
+	    // 응답 객체 생성
+	    Response response = new Response();
+	    response.setStatus(ResponseStatusCode.READ_REPORT_LIST_SUCCESS);
+	    response.setMessage(ResponseMessage.READ_REPORT_LIST_SUCCESS);
+	    response.setData(reports);
+
+	    // HTTP 헤더 설정
+	    HttpHeaders httpHeaders = new HttpHeaders();
+	    httpHeaders.setContentType(new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8")));
+
+	    // ResponseEntity로 반환
+	    ResponseEntity<Response> responseEntity = new ResponseEntity<Response>(response, httpHeaders, HttpStatus.OK);
+	    
+	    return responseEntity;
+	}
+
 	
 	/* [어드민] 신고 상태 변경 (검토중, 완료 등) */
 	@Operation(summary = "관리자 신고 상태 변경")
