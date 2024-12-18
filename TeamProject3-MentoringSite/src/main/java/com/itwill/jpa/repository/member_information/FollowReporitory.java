655412bf0884d248d1a2bd6e6a4125c5bb744582
imun46 <do2816@gmail.com>
2024-12-19 00:36:00 +0900

    fix:(카테고리) 카테고리 번호로 출력 메소드 수정

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/member_information/FollowReporitory.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/member_information/FollowReporitory.java
@@ -18,27 +18,12 @@ public interface FollowReporitory extends JpaRepository<Follow, Long>{
 	
 	/* 멘티, 멘토 번호가 존재하는지 확인 */ 
 	boolean existsByMenteeMember_MemberNoAndMentorMember_MemberNo(Long menteeMemberNo, Long mentorMemberNo);
-	 
-	/* FollowResponseDto 활용하여 원하는 데이터만 출력 */
-	@Query("SELECT new com.itwill.jpa.dto.member_information.FollowResponseDto("
-			+ "f.followNo "
-			+ ",m.memberName "
-			+ ",mp.mentorImage "
-			+ ",c2.categoryName"
-			+ ",c1.categoryName) "
-			+ "FROM Follow f "
-			+ "JOIN f.mentorMember m "
-			+ "JOIN m.mentorProfile mp "
-			+ "JOIN mp.category c1 "
-			+ "JOIN c1.parentCategory c2 "
-			+ "WHERE f.menteeMember.memberNo = :memberNo "
-			+ "ORDER BY m.memberName ASC"
-			)
-	public Page<FollowResponseDto> findFollowMentors(@Param("memberNo") Long memberNo, Pageable pageable);
+
+	/* 팔로워 리스트 */
+	Page<Follow> findByMentorMember_MemberNo(Long memberNo,Pageable pageable);
 	
 	/* 팔로워 멘티 수 찾기 */
 	public Integer countBymentorMember_MemberNo(Long mentorMemberNo);
-
 	
 	/* 멘토보드생성알림을 위한 팔로워찾기 */
 	@Query("SELECT f.menteeMember.memberNo "
--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/member_information/FollowServiceImpl.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/service/member_information/FollowServiceImpl.java
@@ -1,5 +1,6 @@
 package com.itwill.jpa.service.member_information;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -74,14 +75,18 @@ public class FollowServiceImpl implements FollowService{
 	public Page<FollowResponseDto> getMentorList(Long memberNo, int pageNumber, int pageSize){
 		try {
 			Pageable pageable = PageRequest.of(pageNumber, pageSize);
-			Page<FollowResponseDto> followList = followReporitory.findFollowMentors(memberNo, pageable);
-			long totalCount = followList.getTotalElements(); 
+			Page<Follow> follows = followReporitory.findByMentorMember_MemberNo(memberNo,pageable);
+			List<FollowResponseDto> followList = new ArrayList<>();
 			
-			return followList;
+			for (Follow follow : follows) {
+				followList.add(FollowResponseDto.toDto(follow));
+			}
+			
+			return new PageImpl<>(followList, pageable,follows.getTotalElements());
 		} catch (Exception e) {
 			throw new CustomException(ResponseStatusCode.READ_MENTORLIST_FAIL, ResponseMessage.READ_MENTORLIST_FAIL, e);
 		}
-	
+		
 	}
 	
 	/*팔로워 수(멘티 수)*/
