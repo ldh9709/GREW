c37de7f69673df388aa8667475cb907065cf864f
imun46 <do2816@gmail.com>
2024-12-19 17:43:51 +0900

    react:(프로필) 상담내역 수정 중

--- TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/component/Member/mypage/MemberCounselList.jsx
+++ TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/component/Member/mypage/MemberCounselList.jsx
@@ -10,17 +10,22 @@ export default function MemberCounselList() {
   const memberRole = memberCookie.memberRole;
 
   const [counselList, setCounselList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
 
-  const fetchCounselList = async() => {
+  const fetchCounselList = async(page) => {
     try {
-      const response = await chattingApi.listChatRoom(token);
+      const response = await chattingApi.listChatRoom(token, page, 4);
       if (memberRole === 'ROLE_MENTEE') {
-        const filteredCounselList = response.data.filter(item => item.menteeNo === memberNo);
+        const filteredCounselList = response.data.content.filter(item => item.menteeNo === memberNo);
+        console.log(filteredCounselList)
         setCounselList(filteredCounselList);
-      } else if(memberRole === 'ROLE_MENTOR'){
-        const filteredCounselList = response.data.filter(item => item.mentorNo === memberNo);
+        setTotalPages(response.data.totalPages);
       }
-    } catch (error) {
+      // else if (memberRole === 'ROLE_MENTOR') {
+      //   const filteredCounselList = response.data.filter(item => item.mentorNo === memberNo);
+      // }
+      } catch (error) {
       console.log('상담내역 조회 실패', error);
     }
   }
@@ -51,10 +56,20 @@ export default function MemberCounselList() {
   const handleReview = (status) => {
 
   }
-
   useEffect(() => {
-    fetchCounselList();
-  },[])
+    fetchCounselList(currentPage-1);
+  },[currentPage])
+  
+  // 페이지 변경 시 데이터 갱신
+  const paginate = (pageNumber) => {
+      setCurrentPage(pageNumber);
+  };
+
+  // 페이지 번호 버튼 표시
+  const pageNumbers = [];
+  for (let i = 1; i <= totalPages; i++) {
+      pageNumbers.push(i);
+  }
 
   return (
     <div className='tab-content tab-counsel'>
@@ -78,7 +93,14 @@ export default function MemberCounselList() {
             ))}
           </ul>
       )}
-
+      {/* 페이지네이션 버튼 */}
+      <div className="pagenation pagenation-bottom">
+          {pageNumbers.map((number) => (
+              <button key={number} onClick={() => paginate(number)}>
+                  {number}
+              </button>
+          ))}
+      </div>  
     </div>
   )
 }
