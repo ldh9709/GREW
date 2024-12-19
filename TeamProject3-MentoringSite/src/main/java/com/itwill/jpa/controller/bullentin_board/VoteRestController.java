25aed81798d6abdacf550852e02097777da5a3d0
jonadan0322 <jonadan0322>
2024-12-19 18:31:53 +0900

    feat:(추천)인증객체 설정완료

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/bullentin_board/VoteRestController.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/controller/bullentin_board/VoteRestController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itwill.jpa.auth.PrincipalDetails;
 import com.itwill.jpa.dto.alarm.AlarmDto;
 import com.itwill.jpa.dto.bulletin_board.VoteDto;
 import com.itwill.jpa.response.Response;
@@ -23,6 +26,7 @@ import com.itwill.jpa.service.alarm.AlarmService;
 import com.itwill.jpa.service.bullentin_board.VoteService;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/vote")
@@ -33,10 +37,12 @@ public class VoteRestController {
 	private AlarmService alarmService;
 
 	@Operation(summary = "추천")
+	@SecurityRequirement(name = "BearerAuth")//API 엔드포인트가 인증을 요구한다는 것을 문서화(Swagger에서 JWT인증을 명시
 	@PostMapping("/{answerNo}/upvote")
 	public ResponseEntity<Response> upVote(@PathVariable("answerNo") Long answerNo,
-			@RequestParam(name = "memberNo") Long memberNo) {
-		VoteDto savedvote = voteService.upVote(answerNo, memberNo);
+			Authentication authentication) {
+		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+		VoteDto savedvote = voteService.upVote(answerNo, principalDetails.getMemberNo());
 		AlarmDto alarmDto = alarmService.createAlarmByVoteByMentor(answerNo);
 		Response response = new Response();
 		response.setStatus(ResponseStatusCode.CREATED_ANSWER_SUCCESS);
@@ -51,10 +57,12 @@ public class VoteRestController {
 	}
 
 	@Operation(summary = "비추천")
+	@SecurityRequirement(name = "BearerAuth")//API 엔드포인트가 인증을 요구한다는 것을 문서화(Swagger에서 JWT인증을 명시
 	@PostMapping("/{answerNo}/downvote")
 	public ResponseEntity<Response> downVote(@PathVariable("answerNo") Long answerNo,
-			@RequestParam("memberNo") Long memberNo) {
-		VoteDto savedvote = voteService.downVote(answerNo, memberNo);
+			Authentication authentication) {
+		PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+		VoteDto savedvote = voteService.downVote(answerNo, principalDetails.getMemberNo());
 		Response response = new Response();
 		response.setStatus(ResponseStatusCode.CREATED_ANSWER_SUCCESS);
 		response.setMessage("비추천 등록 성공~!");
