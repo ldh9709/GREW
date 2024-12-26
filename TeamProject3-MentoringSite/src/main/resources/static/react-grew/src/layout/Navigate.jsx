b189772ae54d09669da0bdfd41ae3271d2587b31
Joon <wnsgud771@naver.com>
2024-12-26 17:31:29 +0900

    refactor

--- TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/layout/Navigate.jsx
+++ TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/layout/Navigate.jsx
@@ -1,32 +1,15 @@
 import React, { useState } from "react";
 import "../css/styles.css";
 import { useLocation, useNavigate } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Navigate() {
   const [searchTerm, setSearchTerm] = useState(""); // 검색어 상태
   const navigate = useNavigate();
-<<<<<<< HEAD
-  const location = useLocation(); // 문 추가된메서드
-
-  const menuBarStyle = {
-    display: "flex",
-    justifyContent: "left",
-    gap: "20px",
-  };
-  const linkStyle = {
-    fontFamily: "'Noto Sans KR', sans-serif",
-    fontSize: "17px",
-    color: "#555",
-    textDecoration: "none",
-    transition: "color 0.3s",
-    fontWeight: "800",
-  };
-=======
   const location = useLocation();// 문 추가된메서드
   
->>>>>>> refs/heads/master
   // 검색어에 따라 목록 필터링
   const handleSearchChange = (e) => {
     const searchTerm = e.target.value;
@@ -36,9 +19,17 @@ export default function Navigate() {
   const handleSearchClick = () => {
     // 검색어가 있을 경우, searchList 페이지로 이동하면서 검색어를 전달
     if (searchTerm.trim() !== "") {
-      navigate(`/searchList?query=${searchTerm}`); // 검색어를 쿼리 파라미터로 전달
+      if(location.pathname.includes("/mentorprofile/list")) {    //추가된 메서드 문준형
+        //멘토리스트 레이지의 경우
+        navigate(`/mentorprofile/search?query=${searchTerm}`); // 검색어를 쿼리 파라미터로 전달
+      }else{  //추가됨 문준형
+        // 디폴드 질문 검색으로 이동
+        navigate(`/searchList?query=${searchTerm}`); // 검색어를 쿼리 파라미터로 전달
+      
+      } //추가됨  문준형
     }
   };
+
 
   // Enter 키 입력 시 버튼 클릭처럼 동작하도록 처리
   const handleKeyPress = (e) => {
