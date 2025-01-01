0fd0057d1cdfa26d70b5e178476105fd680be088
imun46 <do2816@gmail.com>
2025-01-01 18:37:11 +0900

    react:(어드민) 회원 목록, 멘토 신청 페이지 디자인 완료

--- TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/component/Admin/AdminMemberDetail.jsx
+++ TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/component/Admin/AdminMemberDetail.jsx
@@ -5,51 +5,44 @@ import { faXmark } from '@fortawesome/free-solid-svg-icons'
 import * as adminApi from '../../api/adminApi';
 export default function AdminMemberDetail({mentor, onClose}) {
 
+    console.log(mentor)
     const handleCancel = () => {    
         onClose();
     }
 
   return (
-    <div className="mentor-detail-container">
-        <div className='cancel-button' onClick={handleCancel}>
-            <FontAwesomeIcon icon={faXmark}/>
-        </div>
-        <h2 className="mentor-title">멘토 접수 내용</h2>
-        <div className="mentor-details">
-            <div className="mentor-row">
-                <span className="mentor-label">상태</span>
-                <span className="mentor-value status"></span>
+        <div className="mentor-detail-container">
+            <h2 className="mentor-title">멘토 신청 내역</h2>
+            <div className="mentor-details">
+                <div className="mentor-row">
+                    <span className="mentor-label">전문 분야</span>
+                    <span className="mentor-value">직무상담 &gt; 인사/총무/노무</span>
+                </div>
+                <div className="mentor-row">
+                    <span className="mentor-label">프로필 사진</span>
+                    <div className="mentor-profile-pic"></div>
+                </div>
+                <div className="mentor-row">
+                    <span className="mentor-label">한줄 소개</span>
+                    <span className="mentor-value">안녕하세요 직장에서 힘든 모두 해결해드릴게요!</span>
+                </div>
+                <div className="mentor-row">
+                    <span className="mentor-label">소개글</span>
+                    <textarea className="mentor-textarea" value="안녕하세요 직장에서 힘든 모두 해결해드릴게요!" readOnly></textarea>
+                </div>
+                <div className="mentor-row">
+                    <span className="mentor-label">경력</span>
+                    <div className="mentor-experience">
+                        <p>2021.08 ~ 2021.11</p>
+                        <p>캐논메디칼시스템즈</p>
+                    </div>
+                </div>
             </div>
-            <div className="mentor-row">
-                <span className="mentor-label">신고일자</span>
-                <span className="mentor-value"></span>
-            </div>
-            <div className="mentor-row">
-                <span className="mentor-label">처리일자</span>
-                <span className="mentor-value"></span>
-            </div>
-            <div className="mentor-row">
-                <span className="mentor-label">유형</span>
-                <span className="mentor-value"></span>
-            </div>
-            <div className="mentor-row">
-                <span className="mentor-label">사유</span>
-                <span className="mentor-value"></span>
-            </div>
-            <div className="mentor-detail-content">
-                <span className="mentor-label">내용</span>
-                <textarea
-                className="admin-member-textarea"
-                readOnly
-                />
+            <div className="mentor-actions">
+                <button className="approve-btn">승인</button>
+                <button className="reject-btn">거절</button>
             </div>
         </div>
-        <div className='mentor-detail-button'>
-            <button className="to-resolved"
-            >승인</button>
-            <button className="false-report"
-            >거절</button>
-        </div>
-  </div>
+
   )
 }
