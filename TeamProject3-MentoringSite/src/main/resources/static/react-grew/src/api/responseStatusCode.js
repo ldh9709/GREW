b0ab854885bffebc913503bf68a649796c129edd
ldh9709 <zszz5434@gmail.com>
2025-01-02 23:36:34 +0900

    react : (response) 코드 추가

--- TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/api/responseStatusCode.js
+++ TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/api/responseStatusCode.js
@@ -1,4 +1,4 @@
-// MEMBER 메시지
+// 성공 코드 (2000 ~ 2049)
 export const CREATED_MEMBER_SUCCESS = 2000; // 회원 생성 성공
 export const READ_MEMBER_SUCCESS = 2001; // 회원 조회 성공
 export const READ_MEMBER_LIST_SUCCESS = 2002; // 회원 목록 조회 성공
@@ -11,8 +11,9 @@ export const DEACTIVATE_MEMBER_SUCCESS = 2008; // 회원 비활성화 성공
 export const PASSWORD_RESET_SUCCESS = 2009; // 비밀번호 재설정 성공
 export const EMAIL_SEND_SUCCESS = 2010; // 이메일 발송 성공
 export const INPUTCODE_CONFIRM_SUCCESS = 2011; // 인증 코드 확인 성공
+export const UPDATE_ROLE_SUCCESS = 2012; // 회원 권한 수정 성공
 
-// MEMBER 실패 코드
+// 실패 코드 (2050 ~ )
 export const CREATED_MEMBER_FAIL = 2050; // 회원 생성 실패
 export const INVALID_AUTH_CODE = 2051; // 인증 코드 불일치
 export const READ_MEMBER_FAIL = 2052; // 특정 회원 조회 실패
@@ -24,7 +25,14 @@ export const PASSWORD_RESET_FAIL = 2057; // 비밀번호 재설정 실패
 export const EMAIL_SEND_FAIL = 2058; // 이메일 발송 실패
 export const INPUTCODE_CONFIRM_FAIL = 2059; // 인증 코드 확인 실패
 export const MEMBER_PROVIDER_IS_NOT_EMAIL = 2060; // 이메일로 가입한 회원이 아님
-export const AUTHENTICATION_FAILED = 2061;        // 토큰과 정보가 일치하지 않음
+export const AUTHENTICATION_FAILED = 2061; // 토큰과 정보가 일치하지 않음
+export const UPDATE_ROLE_FAIL = 2062; // 회원 권한 수정 실패
+export const DUPLICATION_MEMBER_ID = 2063; // 아이디 중복
+export const DUPLICATION_MEMBER_EMAIL = 2064; // 이메일 중복
+export const NOT_FOUND_MEMBER = 2065; // 가입하려는 회원정보 null
+export const NOT_AGREEMENT_MEMBER_ID = 2066; // 회원 ID와 정보가 일치하지 않음
+export const NOT_AGREEMENT_MEMBER_NAME = 2067; // 회원 NAME과 정보가 일치하지 않음
+export const INPUT_NULL = 2068; // NULL 값 입력
 
 //MENTOR 성공 코드
 export const CREATED_MENTOR_PROFILE_SUCCESS_CODE = 2350; //멘토 가입 성공
