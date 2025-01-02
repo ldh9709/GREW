c921177dbc8489b882af45f4ec1231d146ea153f
jonadan0322 <jonadan0322>
2025-01-02 18:19:39 +0900

    feat:(알림,채팅)웹소켓 추가중..

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/bullentin_board/AnswerRepository.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/repository/bullentin_board/AnswerRepository.java
@@ -10,106 +10,78 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.itwill.jpa.entity.bullentin_board.Answer;
+
 @Repository
-public interface AnswerRepository extends JpaRepository<Answer, Long>{
-	
-	
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+
 	// 해당 질문에 채택된 답변을 조회
 	@Query("SELECT a FROM Answer a WHERE a.inquiry.inquiryNo = :inquiryNo AND a.answerAccept = 2")
 	Answer findAcceptedAnswerByInquiry(@Param("inquiryNo") Long inquiryNo);
-	
-	/*질문 하나에 달린 답변 리스트*/
-	/*추천순*/
+
+	/* 질문 하나에 달린 답변 리스트 */
+	/* 추천순 */
 	@Query("""
-            SELECT a
-            FROM Answer a
-            JOIN a.inquiry i
-            LEFT JOIN a.votes v 
-            WHERE i.inquiryNo = :inquiryNo
-            AND a.answerStatus = 1
-            GROUP BY a.answerNo, a.answerContent, a.answerDate, a.answerStatus, a.answerAccept, a.member, a.inquiry
-            ORDER BY a.answerAccept DESC, 
-                     (COUNT(CASE WHEN v.voteType = 1 THEN 1 END) - 
-                      COUNT(CASE WHEN v.voteType = 2 THEN 1 END)) DESC
-    """)
-		Page<Answer> findByInquiryAnswerOrderByVotes(@Param("inquiryNo") Long inquiryNo,Pageable pageable);
-	
-	
-	/*최신순*/
-	@Query("SELECT a FROM Answer a " +
-		       "JOIN a.inquiry i " +
-		       "WHERE i.inquiryNo = :inquiryNo " +
-		       "AND a.answerStatus = 1 " +
-		       "ORDER BY a.answerAccept DESC,"+
-		       "a.answerDate DESC")
-		Page<Answer> findByInquiryAnswerOrderByDate(@Param("inquiryNo") Long inquiryNo,Pageable pageable);
+			        SELECT a
+			        FROM Answer a
+			        JOIN a.inquiry i
+			        LEFT JOIN a.votes v
+			        WHERE i.inquiryNo = :inquiryNo
+			        AND a.answerStatus = 1
+			        GROUP BY a.answerNo, a.answerContent, a.answerDate, a.answerStatus, a.answerAccept, a.member, a.inquiry
+			        ORDER BY a.answerAccept DESC,
+			                 (COUNT(CASE WHEN v.voteType = 1 THEN 1 END) -
+			                  COUNT(CASE WHEN v.voteType = 2 THEN 1 END)) DESC
+			""")
+	Page<Answer> findByInquiryAnswerOrderByVotes(@Param("inquiryNo") Long inquiryNo, Pageable pageable);
 
-	
-	/*카테고리별 답변 리스트(검색)*/
-	/*추천순*/
-	@Query("SELECT a FROM Answer a " +
-		       "JOIN a.inquiry i " +
-		       "JOIN i.category c " +
-		       "LEFT JOIN a.votes v " +
-		       "WHERE c.categoryNo = :categoryNo " +
-		       "AND a.answerStatus = 1 " + 
-		       "GROUP BY a.answerNo, a.answerContent, a.answerDate, a.answerStatus, a.answerAccept, a.member, a.inquiry " +
-		       "ORDER BY a.answerAccept DESC, " +
-		       "(COUNT(CASE WHEN v.voteType = 1 THEN 1 END) - " +
-		       "COUNT(CASE WHEN v.voteType = 2 THEN 1 END)) DESC")
-		Page<Answer> findByCategoryAnswerOrderByVotes(@Param("categoryNo") Long categoryNo,Pageable pageable);
+	/* 최신순 */
+	@Query("SELECT a FROM Answer a " + "JOIN a.inquiry i " + "WHERE i.inquiryNo = :inquiryNo "
+			+ "AND a.answerStatus = 1 " + "ORDER BY a.answerAccept DESC," + "a.answerDate DESC")
+	Page<Answer> findByInquiryAnswerOrderByDate(@Param("inquiryNo") Long inquiryNo, Pageable pageable);
 
-	/*최신순*/
-	@Query("SELECT a FROM Answer a " +
-		       "JOIN a.inquiry i " +
-		       "JOIN i.category c " +
-		       "WHERE c.categoryNo = :categoryNo " +
-		       "AND a.answerStatus =1 "+
-		       "ORDER BY a.answerDate DESC")
-		Page<Answer> findByCategoryAnswerOrderByDate(@Param("categoryNo") Long categoryNo,Pageable pageable);
+	/* 카테고리별 답변 리스트(검색) */
+	/* 추천순 */
+	@Query("SELECT a FROM Answer a " + "JOIN a.inquiry i " + "JOIN i.category c " + "LEFT JOIN a.votes v "
+			+ "WHERE c.categoryNo = :categoryNo " + "AND a.answerStatus = 1 "
+			+ "GROUP BY a.answerNo, a.answerContent, a.answerDate, a.answerStatus, a.answerAccept, a.member, a.inquiry "
+			+ "ORDER BY a.answerAccept DESC, " + "(COUNT(CASE WHEN v.voteType = 1 THEN 1 END) - "
+			+ "COUNT(CASE WHEN v.voteType = 2 THEN 1 END)) DESC")
+	Page<Answer> findByCategoryAnswerOrderByVotes(@Param("categoryNo") Long categoryNo, Pageable pageable);
 
-	/*최근 3일동안 추천 많은 답변*/
-	   @Query(value = "SELECT a.* " +
-	            "FROM answer a " +
-	            "LEFT JOIN vote v " +
-	            "ON a.answer_no = v.answer_no " +
-	            "WHERE (v.vote_date >= SYSDATE - INTERVAL '3' DAY OR v.vote_date IS NULL) " +
-	            "AND a.answer_status = 1 " +
-	            "GROUP BY a.answer_no, a.answer_content, a.answer_date, a.answer_status, a.answer_accept, a.inquiry_no, a.member_no " +
-	            "ORDER BY COUNT(CASE WHEN v.vote_type = 1 THEN 1 END) - " +
-	            "COUNT(CASE WHEN v.vote_type = 2 THEN 1 END) DESC," +
-	            "a.answer_no DESC", 
-            nativeQuery = true)//jpql엔 sysdate 사용불가하기 때문에 nativeQuery로 오라클의 sql사용
-		List<Answer> findByAnswerOrderByVoteDate();
+	/* 최신순 */
+	@Query("SELECT a FROM Answer a " + "JOIN a.inquiry i " + "JOIN i.category c " + "WHERE c.categoryNo = :categoryNo "
+			+ "AND a.answerStatus =1 " + "ORDER BY a.answerDate DESC")
+	Page<Answer> findByCategoryAnswerOrderByDate(@Param("categoryNo") Long categoryNo, Pageable pageable);
 
-	@Query("SELECT i.member.memberNo "
-		       + "FROM Answer a "
-		       + "JOIN a.inquiry i "
-		       + "WHERE a.answerNo = :answerNo")
-		Long findByMemberNoByInquiryByAnswer(@Param("answerNo")Long answerNo);
+	/* 최근 3일동안 추천 많은 답변 */
+	@Query(value = "SELECT a.* " + "FROM answer a " + "LEFT JOIN vote v " + "ON a.answer_no = v.answer_no "
+			+ "WHERE (v.vote_date >= SYSDATE - INTERVAL '3' DAY OR v.vote_date IS NULL) " + "AND a.answer_status = 1 "
+			+ "GROUP BY a.answer_no, a.answer_content, a.answer_date, a.answer_status, a.answer_accept, a.inquiry_no, a.member_no "
+			+ "ORDER BY COUNT(CASE WHEN v.vote_type = 1 THEN 1 END) - "
+			+ "COUNT(CASE WHEN v.vote_type = 2 THEN 1 END) DESC," + "a.answer_no DESC", nativeQuery = true)
+	List<Answer> findByAnswerOrderByVoteDate();
 
-	//내가 작성한 답변내역
-	@Query("SELECT a FROM Answer a WHERE a.member.memberNo = :memberNo AND a.answerStatus = 1 " +
-		       "ORDER BY a.answerDate DESC")
+
+	@Query("SELECT i.member.memberNo " + "FROM Answer a " + "JOIN a.inquiry i " + "WHERE a.answerNo = :answerNo")
+	Long findByMemberNoByInquiryByAnswer(@Param("answerNo") Long answerNo);
+
+	// 내가 작성한 답변내역
+	@Query("SELECT a FROM Answer a WHERE a.member.memberNo = :memberNo AND a.answerStatus = 1 "
+			+ "ORDER BY a.answerDate DESC")
 	Page<Answer> findByMemberMemberNoOrderByAnswerDateDesc(@Param("memberNo") Long memberNo, Pageable pageable);
 
 	// 답변 내용(answer_content) 또는 질문 제목(inquiry_title)에서 검색어를 포함하는 답변 찾기
 
-    @Query("SELECT a FROM Answer a " +
-           "JOIN a.inquiry i " +
-           "JOIN i.category c " +
-           "LEFT JOIN a.votes v " +
-           "WHERE (a.answerContent LIKE %:search% OR i.inquiryTitle LIKE %:search%) " +
-           "AND a.answerStatus = 1 " +
-           "GROUP BY a.answerNo, a.answerContent, a.answerDate, a.answerStatus, a.answerAccept, a.member, a.inquiry " +
-           "ORDER BY a.answerAccept DESC, " +
-           "(COUNT(CASE WHEN v.voteType = 1 THEN 1 END) - " +
-           "COUNT(CASE WHEN v.voteType = 2 THEN 1 END)) DESC")
-    Page<Answer> searchAnswersByKeyword(@Param("search") String search, Pageable pageable);
-    
-    //질문의 답변 수
-    Long countByInquiry_InquiryNoAndAnswerStatus(Long inquiryNo, Integer answerStatus);
+	@Query("SELECT a FROM Answer a " + "JOIN a.inquiry i " + "JOIN i.category c " + "LEFT JOIN a.votes v "
+			+ "WHERE (a.answerContent LIKE %:search% OR i.inquiryTitle LIKE %:search%) " + "AND a.answerStatus = 1 "
+			+ "GROUP BY a.answerNo, a.answerContent, a.answerDate, a.answerStatus, a.answerAccept, a.member, a.inquiry "
+			+ "ORDER BY a.answerAccept DESC, " + "(COUNT(CASE WHEN v.voteType = 1 THEN 1 END) - "
+			+ "COUNT(CASE WHEN v.voteType = 2 THEN 1 END)) DESC")
+	Page<Answer> searchAnswersByKeyword(@Param("search") String search, Pageable pageable);
 
-    Answer findByInquiry_InquiryNoAndMember_MemberNo(Long inquiryNo,Long memberNo);
+	// 질문의 답변 수
+	Long countByInquiry_InquiryNoAndAnswerStatus(Long inquiryNo, Integer answerStatus);
+
+	Answer findByInquiry_InquiryNoAndMember_MemberNo(Long inquiryNo, Long memberNo);
 
 }
--- TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/component/Chatting/ChatAlarim.jsx
+++ TeamProject3-MentoringSite/src/main/resources/static/react-grew/src/component/Chatting/ChatAlarim.jsx
@@ -1,5 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "../../css/chatApp.css"; // CSS를 별도 파일로 분리
+import SockJS from "sockjs-client";
+import { Client as StompClient } from "@stomp/stompjs";
 import ChattingMessage from "./Chatting.jsx";
 import ChatRoom from "./ChatRoom.jsx";
 import Alarim from "../Alarim.jsx";
@@ -12,7 +14,42 @@ const ChatAlarim = () => {
   const [activePanel, setActivePanel] = useState(null); //패널을 열때 chat 또는 notification인지 구분하는 용도
   const [roomId, setRoomId] = useState(null); // 선택된 roomId 상태
   const [roomName, setRoomName] = useState(null); // 선택된 roomName 상태
-  const { token , member } = useMemberAuth();
+  const { token, member } = useMemberAuth();
+  let stompClient = useRef(null);
+  // stompClient 초기화 및 WebSocket 연결
+  
+  useEffect(() => {
+    if (roomId && member.memberName) {
+      const socket = new SockJS(`http://localhost:8080/chat`);
+      stompClient.current = new StompClient({
+        webSocketFactory: () => socket,
+        onConnect: () => {
+          stompClient.current.subscribe(
+            `/topic/messages/member/${member.memberNo}`,
+            (response) => {
+              // 서버에서 소켓내용을 받아옴
+              const message = JSON.parse(response.body);
+              if (!message.chatMessageDate) {
+                message.chatMessageDate = new Date().toISOString(); // ISO 8601 형식으로 현재 시간 설정
+              }
+              const messageType =
+                message.memberName === member.memberName ? "sent" : "received";
+            }
+          );
+        },
+        onDisconnect: () => console.log("Disconnected"),
+      });
+
+      stompClient.current.activate();
+
+      return () => {
+        if (stompClient.current) {
+          stompClient.current.deactivate(); // 소켓이 연결되었다면 일을 끝낸 뒤 다시 연결 종료
+        }
+      };
+    }
+    console.log("소켓검사 종료");
+  }, [roomId]);
 
   const togglePanel = (panel) => {
     //chat 또는 notification를 확인하여 슬라이드 패널을 열고 닫음
@@ -30,19 +67,14 @@ const ChatAlarim = () => {
     }
   };
   useEffect(() => {
-    if(member){
-
-        alarmIsReadCount(); // 데이터를 가져오는 함수
-        const intervalId = setInterval(() => {
-            alarmIsReadCount(); // 데이터를 가져오는 함수
-        }, 3000); // 5000ms = 5초
-        return () => clearInterval(intervalId);
+    if (member) {
+      alarmIsReadCount(); // 데이터를 가져오는 함수
     }
-  }, [member]);
-  
+  }, []);
+
   return (
     <div>
-      {member&&member.memberNo != null ? (
+      {member && member.memberNo != null ? (
         <>
           {/* Panel */}
           <div className={`chat-panel ${activePanel ? "open" : ""}`}>
