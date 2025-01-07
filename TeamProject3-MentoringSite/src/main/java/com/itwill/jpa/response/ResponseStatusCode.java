c45ea7f04e1470baa8885cc61a20310dbbebfe5b
Joon <wnsgud771@naver.com>
2025-01-07 16:29:14 +0900

    Merge branch 'master' of https://github.com/2024-07-JAVA-DEVELOPER-155/final-project-team2.git

--- TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/response/ResponseStatusCode.java
+++ TeamProject3-MentoringSite/src/main/java/com/itwill/jpa/response/ResponseStatusCode.java
@@ -62,12 +62,12 @@ public class ResponseStatusCode {
 	public static final int MEMBER_NOT_FOUND = 2370; // 멤버를 찾을 수 없음 
 	public static final int NOT_A_MENTOR = 2375; // 멘토가 아님
 	/******************* MENTOR PROFILE 메시지 코드 ************************/
-    public static final int CREATED_MENTOR_PROFILE_SUCCESS_CODE = 2350;
+    public static final int CREATED_MENTOR_PROFILE_SUCCESS_CODE = 2351;
     public static final int READ_MENTOR_PROFILE_SUCCESS_CODE = 2355;
-    public static final int READ_MENTOR_PROFILE_LIST_SUCCESS_CODE = 2360;
-    public static final int UPDATE_MENTOR_PROFILE_SUCCESS_CODE = 2365;
-    public static final int DELETE_MENTOR_PROFILE_SUCCESS_CODE = 2370;
-    public static final int MENTOR_PROFILE_NOT_FOUND_CODE = 2375;
+    public static final int READ_MENTOR_PROFILE_LIST_SUCCESS_CODE = 2361;
+    public static final int UPDATE_MENTOR_PROFILE_SUCCESS_CODE = 2361;
+    public static final int DELETE_MENTOR_PROFILE_SUCCESS_CODE = 2371;
+    public static final int MENTOR_PROFILE_NOT_FOUND_CODE = 2376;
     public static final int READ_MENTOR_PROFILE_LIST_FAIL_CODE = 2380;
     public static final int MENTOR_PROFILE_STATUS_UPDATE_SUCCESS_CODE = 2385;
     public static final int MENTOR_PROFILE_STATUS_UPDATE_FAIL_CODE = 2390;
